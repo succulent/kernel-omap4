11cce30651cebd5c9f326f1766329b7d8b1d09fe
Rob Clark <rob@ti.com>
2012-03-16 14:37:56 -0500

    squash! nouveau: add prime support.

--- drivers/gpu/drm/nouveau/nouveau_drv.c
+++ drivers/gpu/drm/nouveau/nouveau_drv.c
@@ -431,8 +431,10 @@ static struct drm_driver driver = {
 	.ioctls = nouveau_ioctls,
 	.fops = &nouveau_driver_fops,
 
-	.prime_export = nouveau_gem_prime_export,
-	.prime_import = nouveau_gem_prime_import,
+	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
+	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
+	.gem_prime_export = nouveau_gem_prime_export,
+	.gem_prime_import = nouveau_gem_prime_import,
 
 	.gem_init_object = nouveau_gem_object_new,
 	.gem_free_object = nouveau_gem_object_del,
