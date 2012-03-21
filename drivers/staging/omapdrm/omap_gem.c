fb3308880f93701552b849c0ce38c36b8e2d3422
Rob Clark <rob@ti.com>
2012-03-21 18:48:30 -0500

    Revert "WIP: tiler/2d support for DSS"
    This reverts commit b92e79b5032244e56bbbb37e9469e9241f8563f7.

--- drivers/staging/omapdrm/omap_gem.c
+++ drivers/staging/omapdrm/omap_gem.c
@@ -277,12 +277,6 @@ static void omap_gem_detach_pages(struct drm_gem_object *obj)
 	omap_obj->pages = NULL;
 }
 
-/* get buffer flags */
-uint32_t omap_gem_flags(struct drm_gem_object *obj)
-{
-	return to_omap_bo(obj)->flags;
-}
-
 /** get mmap offset */
 static uint64_t mmap_offset(struct drm_gem_object *obj)
 {
@@ -770,26 +764,6 @@ int omap_gem_put_paddr(struct drm_gem_object *obj)
 		}
 	}
 fail:
-	mutex_unlock(&obj->dev->struct_mutex);
-	return ret;
-}
-
-/* Get rotated scanout address (only valid if already pinned), at the
- * specified orientation and x,y offset from top-left corner of buffer
- * (only valid for tiled 2d buffers)
- */
-int omap_gem_rotated_paddr(struct drm_gem_object *obj, uint32_t orient,
-		int x, int y, dma_addr_t *paddr)
-{
-	struct omap_gem_object *omap_obj = to_omap_bo(obj);
-	int ret = -EINVAL;
-
-	mutex_lock(&obj->dev->struct_mutex);
-	if ((omap_obj->paddr_cnt > 0) && omap_obj->block &&
-			(omap_obj->flags & OMAP_BO_TILED)) {
-		*paddr = tiler_tsptr(omap_obj->block, orient, x, y);
-		ret = 0;
-	}
 	mutex_unlock(&obj->dev->struct_mutex);
 	return ret;
 }
